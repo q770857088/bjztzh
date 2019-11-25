1c33c04bace98bc6fee4560b24516de2cbe94c29
wangdi <B>
2019-11-25 17:43:46 +0800

    edit mysql conifg

--- model/config.php
+++ model/config.php
@@ -1,7 +1,9 @@
 <?php 
 define("DB_HOST",'localhost');
-define("DB_USER",'root');
-define('DB_PWD','root');
+define("DB_USER",'db_cro');
+//define("DB_USER",'root');
+//define('DB_PWD','root');
+define('DB_PWD','RbeeiJe2AA3mP7cJ');
 define('DB_NAME','db_cro');
 define('DB_PORT','3306');
 define('DB_TYPE','mysql');
